b4c8b6edacd90408b0b317b3cc1efa6e62a3efdf
Djvon Simpson <55302995+dsimp@users.noreply.github.com>
2024-10-13 17:37:57 +0000

    Done edit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,80 +1,65 @@
 class MoviesController < ApplicationController
   def new
-    @the_movie = Movie.new
-
-    render template: "movies/new"
+    @movie = Movie.new
   end
 
   def index
-    matching_movies = Movie.all
 
-    @list_of_movies = matching_movies.order({ :created_at => :desc })
+    @movies = Movie.order(created_at: :desc )
 
     respond_to do |format|
       format.json do
-        render json: @list_of_movies
+        render json: @movies
       end
 
-      format.html do
-        render({ :template => "movies/index" })
-      end
+      format.html 
     end
   end
 
   def show
-    the_id = params.fetch(:id)
-
-    matching_movies = Movie.where({ :id => the_id })
-
-    @the_movie = matching_movies.first
-
-    render({ :template => "movies/show" })
+    # @movie = Movie.find_by(id: params.fetch(:id))
+    @movie = Movie.find(params.fetch(:id))
   end
 
   def create
-    @the_movie = Movie.new
-    @the_movie.title = params.fetch("query_title")
-    @the_movie.description = params.fetch("query_description")
+    movie_attributes = params.required(:movie).permit(:title, :description)
+    @movie = Movie.new(movie_attributes)
+    # movie_attributes = params.fetch(:movie)
+    # @movie = Movie.new(movie_attributes)
+    # @movie.title = params.fetch(:movie).fetch(:title)
+    # @movie.description = params.fetch(:movie).fetch(:description)
 
-    if @the_movie.valid?
-      @the_movie.save
-      redirect_to("/movies", { :notice => "Movie created successfully." })
+    if @movie.valid?
+      @movie.save
+      redirect_to movies_url, :notice => "Movie created successfully."  
     else
-      render template: "movies/new"
+      render "movies/new"
     end
   end
 
   def edit
-    the_id = params.fetch(:id)
-
-    matching_movies = Movie.where({ :id => the_id })
-
-    @the_movie = matching_movies.first
-
-    render({ :template => "movies/edit" })
+    @movie = Movie.find_by(id: params.fetch(:id))
   end
 
   def update
-    the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    movie = Movie.find(params.fetch(:id))
 
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
+    movie.title = params.fetch("title")
+    movie.description = params.fetch("description")
 
-    if the_movie.valid?
-      the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully."} )
+    if movie.valid?
+      movie.save
+      redirect_to movie_url(movie), :notice => "Movie updated successfully." 
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => "Movie failed to update successfully." })
+      redirect_to movie_url(movie), :alert => "Movie failed to update successfully." 
     end
   end
 
   def destroy
-    the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    movie = Movie.find(params.fetch(:id))
 
-    the_movie.destroy
+    movie.destroy
 
-    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+    redirect_to movies_url, :notice => "Movie deleted successfully."  
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,31 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "movies", :action => "index" })
+  # get "/" => "movies#index"
+  resources :movies
+  # root "movies#index"
+  # post "/movies" => "movies#create", as: :movies #plural
+  # get "/movies/new" => "movies#new", as: :new_movie
+  # get "/movies" => "movies#index"
+  # get "/movies/:id" => "movies#show", as: :movie #single
+  # patch "/movies/:id" => "movies#update"
+  # get "/movies/:id/edit" => "movies#edit", as: :edit_movie
+  # delete "/movies/:id" => "movies#destroy"
 
   # Routes for the Movie resource:
 
   # CREATE
-  post("/movies", { :controller => "movies", :action => "create" })
-  get("/movies/new", { :controller => "movies", :action => "new" })
+  # post"/movies", { :controller => "movies", :action => "create" }
+  # get"/movies/new", { :controller => "movies", :action => "new" }
           
-  # READ
-  get("/movies", { :controller => "movies", :action => "index" })
-  get("/movies/:id", { :controller => "movies", :action => "show" })
+  # # READ
+  # get"/movies", { :controller => "movies", :action => "index" }
+  # get"/movies/:id", { :controller => "movies", :action => "show" }
   
-  # UPDATE
-  patch("/movies/:id", { :controller => "movies", :action => "update" })
-  get("/movies/:id/edit", { :controller => "movies", :action => "edit" })
+  # # UPDATE
+  # patch"/movies/:id", { :controller => "movies", :action => "update" }
+  # get"/movies/:id/edit", { :controller => "movies", :action => "edit" }
   
-  # DELETE
-  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
+  # # DELETE
+  # delete"/movies/:id", { :controller => "movies", :action => "destroy" }
 
   #------------------------------
 end
